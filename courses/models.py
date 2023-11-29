f2d0ecf139911218a07d823c633baec9539f2c15
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-11-29 14:00:26 +0600

    Add field video_url to model Lesson

--- courses/models.py
+++ courses/models.py
@@ -13,4 +13,5 @@ class Lesson(models.Model):
     title = models.CharField(max_length=300, verbose_name='Title')
     preview = models.ImageField(upload_to='courses/', verbose_name='Preview', **NULLABLE)
     description = models.TextField(verbose_name='Description', **NULLABLE)
+    video_url = models.URLField(verbose_name='Link to video', **NULLABLE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Course')
