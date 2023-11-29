f6af73ed814066b574c67d3ded1529d4b1d33d78
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-11-29 23:47:52 +0600

    End-points and serializers for all models created

--- courses/models.py
+++ courses/models.py
@@ -8,6 +8,13 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='courses/', verbose_name='Preview', **NULLABLE)
     description = models.TextField(verbose_name='Description', **NULLABLE)
 
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
 
 class Lesson(models.Model):
     title = models.CharField(max_length=300, verbose_name='Title')
@@ -15,3 +22,10 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Description', **NULLABLE)
     video_url = models.URLField(verbose_name='Link to video', **NULLABLE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Course')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Lesson'
+        verbose_name_plural = 'Lessons'
