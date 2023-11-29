e0e13276cf6cdbf02977d2b791d57cb4bd776596
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-11-29 13:57:30 +0600

    Courses and Users apps created with models

---
+++ courses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Title')
+    preview = models.ImageField(upload_to='courses/', verbose_name='Preview', **NULLABLE)
+    description = models.TextField(verbose_name='Description', **NULLABLE)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Title')
+    preview = models.ImageField(upload_to='courses/', verbose_name='Preview', **NULLABLE)
+    description = models.TextField(verbose_name='Description', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Course')
