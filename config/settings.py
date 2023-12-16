c4ea6aff5b186fd8f719090506b86eeb8991858e
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-12-16 18:21:50 +0600

    Swagger configured and working via API key

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'drf_yasg',
 
     'users',
     'courses',
@@ -151,6 +152,16 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
 }
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
